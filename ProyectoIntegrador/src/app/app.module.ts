a2ab8e80fe5f437c567fb9d7503ff11df2401fb1
Daniel Ulrich <121643833+MeTuuL@users.noreply.github.com>
2023-03-29 13:20:57 -0300

    Front-End terminado

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { CurriculumComponent } from './components/curriculum/curriculum.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoApComponent,
+    BannerComponent,
+    ProfileComponent,
+    NavbarComponent,
+    SobremiComponent,
+    SkillsComponent,
+    CurriculumComponent,
+    ProyectoComponent,
+    ContactoComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
